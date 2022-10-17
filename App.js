3145b181c34cec0080f733b54935bccfc3497f96
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-17 10:31:11 +0600

    added initial setup

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Text from './src/components/text/text';
+import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
+import { typography } from './src/theme/typography';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Font is loading...</Text>
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: typography.bold, fontSize: spacing[5]}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.darkOrange,
     alignItems: 'center',
     justifyContent: 'center',
   },
