8593d4e2f28dec573c19b46b09cb634a86f154c8
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-18 16:38:54 +0600

    complete

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,34 +1,61 @@
-import { Entypo } from '@expo/vector-icons';
-import React from "react";
-import { FlatList, Pressable, StyleSheet, View } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Entypo } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { FlatList, Pressable, StyleSheet, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import PlanetHeader from "../components/planet-header";
 import Text from "../components/text/text";
 import { PLANET_LISTS } from "../data/planet-list";
 import { colors } from "../theme/colors";
 import { spacing } from "../theme/spacing";
 
+const Home = ({ navigation }) => {
+  const [lists, setLists] = useState(PLANET_LISTS);
+
+  const searchFilter = (text) => {
+
+    const filteredLists = PLANET_LISTS.filter(planet => {
+      
+      const planetName = planet.name.toLowerCase();
+      const userTypedText = text.toLowerCase();
+      return planetName.includes(userTypedText);
+    })
+    setLists(filteredLists);
+  }
 
 
-const Home = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
 
+      <TextInput
+        placeholder="Type the planet name"
+        style={styles.searchInput}
+        placeholderTextColor={colors.white}
+        autoCorrect={false}
+        onChangeText={(text) => searchFilter(text)}
+      />
+
       <FlatList
         contentContainerStyle={styles.list}
-        data={PLANET_LISTS}
+        data={lists}
         keyExtractor={(item, index) => item.name}
-        renderItem={({item, index}) => {
+        renderItem={({ item, index }) => {
           return (
-            <Pressable onPress={() => {
-              navigation.navigate('Details', {planet: item})
-            }} style={styles.item}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <View style={[styles.circle, {backgroundColor: item.color}]} />
-                    <Text preset="h4" style={styles.itemName}>{item.name}</Text>
-                </View>
-                <Entypo name="chevron-small-right" size={18} color="white" />
+            <Pressable
+              onPress={() => {
+                navigation.navigate("Details", { planet: item });
+              }}
+              style={styles.item}
+            >
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View
+                  style={[styles.circle, { backgroundColor: item.color }]}
+                />
+                <Text preset="h4" style={styles.itemName}>
+                  {item.name}
+                </Text>
+              </View>
+              <Entypo name="chevron-small-right" size={18} color="white" />
             </Pressable>
           );
         }}
@@ -41,30 +68,37 @@ const Home = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.black
+    backgroundColor: colors.black,
   },
   item: {
     flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: spacing[4]
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: spacing[4],
   },
   list: {
-    padding: spacing[4]
+    padding: spacing[4],
   },
   circle: {
     width: 20,
     height: 20,
-    borderRadius: 15
+    borderRadius: 15,
   },
   itemName: {
-    textTransform: 'uppercase',
-    marginLeft: spacing[4]
+    textTransform: "uppercase",
+    marginLeft: spacing[4],
   },
   separator: {
     borderBottomColor: colors.white,
-    borderBottomWidth: 0.3
-  }
+    borderBottomWidth: 0.3,
+  },
+  searchInput: {
+    padding: spacing[4],
+    color: colors.white,
+    borderBottomColor: colors.white,
+    borderBottomWidth: 1,
+    margin: spacing[5],
+  },
 });
 
 export default Home;
